175f8a697130c759bd097c4d353e98674432dfbf
gpjua <gpjua@Juan>
2024-03-11 21:09:20 -0600

    playlis 07 marzo Para profesor alfonso, senseeeeei

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author gpjua
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas p1=new Musicas("Baby Blue", "Hasta el fin del mundo", "2019", 4);
+        p1.ShowPlaylist();
+    }
+}
